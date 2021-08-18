603342752f447b31812e17601b71acac2f3b6957
vetrisvl <89128013+vetrisvl@users.noreply.github.com>
2021-08-18 19:10:18 +0530

    Add files via upload

---
+++ Question 10.js
@@ -0,0 +1,6 @@
+function Hero(name,level)
+{
+this.name=name;
+this.level=level;
+}
+this keyword is used in a constructor function,this refers to object.
---
+++ Question 9.js
@@ -0,0 +1,9 @@
+const gimli=
+{
+    name:"Gimli",
+    race:"dwarf",
+    weapon:"axe",
+    greet:function() {
+      return 'Hi,my name id ${this.name}!';},
+ };
+ console.log(gimli.greet());
