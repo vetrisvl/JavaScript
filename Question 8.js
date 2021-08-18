f5835baaf88a564fec935e7f94fbc6a88d4f8e9c
vetrisvl <89128013+vetrisvl@users.noreply.github.com>
2021-08-18 19:09:07 +0530

    Delete Question 8.js

--- Question 8.js
+++
@@ -1,9 +0,0 @@
-const name={ 
-   firstName:'philip',
-   lastName:'fry'
-   };
-const details={
-   job:'Delivery Boy',
-   employer:'planet express'
-   };
-const character=object.assign(name,values);
